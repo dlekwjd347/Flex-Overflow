594847eb87dafc75f930878e7ec4fe330053a7c7
Johnson Pham <mrjohnsonpham@gmail.com>
2020-09-12 18:49:30 -0400

    removed public from main handlebars

--- models/askquestions.js
+++ models/askquestions.js
@@ -2,10 +2,7 @@ module.exports = function (sequelize, DataTypes) {
     const AskQuestions = sequelize.define("AskQuestions", {
         title: {
             type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
+            allowNull: false
         }
     });
     return AskQuestions;
