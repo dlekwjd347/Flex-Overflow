2d90ac00386ae0a5fb8ca322650afabc644dfe4d
Bobby Sporman <bobby.sporman@yahoo.com>
2020-09-13 10:42:38 -0400

    updating because Anthony said it was behond

--- models/index.js
+++ models/index.js
@@ -3,7 +3,9 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
+console.log(process.env.NODE_ENV);
 const env = process.env.NODE_ENV || "development";
+
 const config = require(__dirname + "/../config/config.json")[env];
 const sequelize = config.use_env_variable ? new Sequelize(process.env[config.use_env_variable], config) : new Sequelize(config.database, config.username, config.password, config);
 let db = {sequelize, Sequelize};
